955e2b019b8856a29e18d2fdaaa64fe0e3bd1ed7
nqkpro96 <nqkpro96@gmail.com>
2017-05-09 03:51:50 +0700

    Update

--- admin.py
+++ admin.py
@@ -11,6 +11,9 @@ class account(object):
         self.name = name
         self.password = password
         self.usbid = usbid
+        self.blockStatus = False
+    def getName(self):
+        return self.name
     def printUser(self):
         print self.name
         # print "USB: ", self.usbid
@@ -29,17 +32,58 @@ class account(object):
     def menu(self):
         pass
 class user(account):
-    def menu(self):
-        print self.usbid
+    pass
 
 class admin(account):
-     
+    def __init__(self, (name, password, usbid)):
+        self.name = name
+        self.password = password
+        self.usbid = usbid
+        self.listUser = None
+    def search(self, name):
+        for i in self.listUser:
+            if i.getName() == name:
+                print i.getName()
+                return i
+        return None
+    def register(self):
+        self.listUser.register()
+    def delete(self, name):
+        self.listUser.delete(name)
+    def block(self, name):
+        for i in self.listUser:
+            if i.getName() == name:
+                if i.blockStatus == False:
+                    i.blockStatus = True
+                else:
+                    i.blockStatus = False
+        return None
+    def menu(self):
+        print "1. Register new user"
+        print "2. Delete user"
+        print "3. Block user"
+        print "4. Print all username"
+        choose = int(raw_input("Choose :"))
+        if choose == 1:
+            self.listUser.register()
+        elif choose == 2:
+            name = raw_input("Name account to be delete:")
+            self.listUser.delete(name)
+        elif choose == 3:
+            name = raw_input("Name account to be block/unblock:")
+            self.block(name)
+        elif choose == 4:
+            # name = raw_input("Name account to be delete:")
+            self.listUser.printList()
+
+
 class listUser(object):
     def __init__(self, name):
         self.name = name
     accountList = []
     def register(self):
         self.accountList.append(user(input()))
+
     def printList(self):
         for i in self.accountList:
             print '%d.' %(self.accountList.index(i)+1) ,
@@ -71,13 +115,20 @@ class listUser(object):
                 temp = i
                 return i.authen(inp[1], inp[2])
         if temp == None:
-            print "Not found user!"
+            print "Not found account!"
+    def delete(self, name):
+        for i in self.accountList:
+            if i.getName() == name:
+                self.accountList.remove(i)
 
 
 def main():
     ls = listUser("ls")
     ls.loadList("user.obj")
-    # ls.printList()
-    print ls.authen()
+    root = admin(("root", None, None))
+    root.listUser = ls
+    root.menu()
+    ls.saveList("user.obj")
+    # ls.authen()
 
 main()
--- usb.py
+++ usb.py
@@ -1,49 +1,44 @@
-
-import pyudev
-from time import strftime
-
-current_devices={}
-info = None
-
-context = pyudev.Context()
-monitor = pyudev.Monitor.from_netlink(context)
-monitor.filter_by(subsystem='usb', device_type='usb_device')
-
-
-
-
-def print_device_event(device):
-    
-    global current_devices
-    
-    if device.action == 'add':
-        print("Device {0[ID_VENDOR]} {0[ID_MODEL]} with serial number {0[ID_SERIAL]} was plugged in. ----- {0[DEVNAME]}".format(device))
-        
-        observer.send_stop()
-        return {device['DEVNAME'],device['ID_VENDOR'], device['ID_SERIAL']}
-        # print device['ID_VENDOR']
-    if device.action == 'remove':
-        print("device removed")
-    
-    
-    
-def process_device_event(device):
-
-    global current_devices, observer, info
-    
-    info = print_device_event(device)
-    # observer.stop()
-    # print info
-    
-observer = pyudev.MonitorObserver(monitor, callback=process_device_event, name='monitor-observer')
-observer.daemon = False
-
-
-
 def main():
-    
+	import pyudev
+	from time import strftime
 
-    observer.start()
-    
-    
-main()
+	current_devices={}
+	info = None
+
+	context = pyudev.Context()
+	monitor = pyudev.Monitor.from_netlink(context)
+	monitor.filter_by(subsystem='usb', device_type='usb_device')
+
+
+
+
+	def print_device_event(device):
+	    
+	    global current_devices
+	    
+	    if device.action == 'add':
+	        print("Device {0[ID_VENDOR]} {0[ID_MODEL]} with serial number {0[ID_SERIAL]} was plugged in. ----- {0[DEVNAME]}".format(device))
+	        
+	        observer.send_stop()
+	        return {device['DEVNAME'],device['ID_VENDOR'], device['ID_SERIAL']}
+	        # print device['ID_VENDOR']
+	    if device.action == 'remove':
+	        print("device removed")
+	    
+	    
+	    
+	def process_device_event(device):
+
+	    global current_devices, observer, info
+	    
+	    info = print_device_event(device)
+	    # observer.stop()
+	    return info
+	    
+	observer = pyudev.MonitorObserver(monitor, callback=process_device_event, name='monitor-observer')
+	observer.daemon = False
+	observer.start()
+	    
+	    
+print main()
+
